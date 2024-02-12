0462bd50c9a699d01d4236fc92dc7cf2d66e3649
druzhee <mohannadsa54@gmail.com>
2024-02-12 15:15:59 +0100

    Models

---
+++ crypto-scanner.Data/Models/RootCoin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crypto_scanner.Data.Models
+{
+    internal class RootCoin
+    {
+    }
+}
