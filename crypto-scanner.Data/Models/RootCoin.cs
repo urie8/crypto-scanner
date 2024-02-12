0221f69428e0e935c995067d157f8c175aab43fd
druzhee <mohannadsa54@gmail.com>
2024-02-12 15:21:02 +0100

    mohabranch

--- crypto-scanner.Data/Models/RootCoin.cs
+++ crypto-scanner.Data/Models/RootCoin.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace crypto_scanner.Data.Models
 {
-    internal class RootCoin
+    public class RootCoin
     {
+        public class Root 
+        {
+            [Key]
+            [JsonProperty("id")] 
+            public int Id { get; set; } 
+            [JsonProperty("symbol")] 
+            public string? Symbol { get; set; } 
+            [JsonProperty("name")] 
+            public string? Name { get; set; } 
+        }
     }
 }
