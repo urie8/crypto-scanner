1e4bc15101b27929992a392cedd8d82504bc52aa
urie8 <dionpeci08@gmail.com>
2024-02-13 13:22:32 +0100

    Added api methods for single coin and list of coins

--- crypto-scanner.Data/CryptoRepo.cs
+++ crypto-scanner.Data/CryptoRepo.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace crypto_scanner.Data
+﻿namespace crypto_scanner.Data
 {
-    internal class CryptoRepo
+    public class CryptoRepo
     {
     }
 }
