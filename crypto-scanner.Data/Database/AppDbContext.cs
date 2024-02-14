7b4bb1a9d906f1325b265b98cb9c522b4df8ae65
druzhee <mohannadsa54@gmail.com>
2024-02-14 09:28:20 +0100

    done

--- crypto-scanner.Data/Database/AppDbContext.cs
+++ crypto-scanner.Data/Database/AppDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using crypto_scanner.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace crypto_scanner.Data.Database
 {
@@ -9,7 +12,31 @@ namespace crypto_scanner.Data.Database
         {
 
         }
+
         public DbSet<RootCoin> Coins { get; set; }
     }
+    //public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    //{
+    //    public AppDbContext CreateDbContext(string[] args)
+    //    {
+    //        // Build configuration
+    //        IConfigurationRoot configuration = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile("appsettings.json")
+    //            .Build();
+
+    //        // Create options builder using SQL Server from configuration
+    //        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+    //        var connectionString = configuration.GetConnectionString("DbConnection");
+    //        optionsBuilder.UseSqlServer(connectionString);
+
+    //        return new AppDbContext(optionsBuilder.Options);
+    //    }
+    //}
+    //public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    //{
+    //    public DbSet<RootCoin> Coins { get; set; }
+    //}
+
 }
 
---
+++ crypto-scanner.Data/Migrations/20240214082056_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crypto_scanner.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coins",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coins", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coins");
+        }
+    }
+}
